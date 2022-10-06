1de239d26fae0a2b64502cc759617ecb4c7e2a41
x183 <palmoscar4@gmail.com>
2022-10-06 13:13:47 +0200

    Can now play albums and artists

--- backend/src/routers/playRouter.ts
+++ backend/src/routers/playRouter.ts
@@ -1,12 +1,23 @@
 import { Router } from 'express';
-import { playSong } from '../services/spotifyService.js';
+import { playAlbum, playArtist, playSong } from '../services/spotifyService.js';
 
 const playRouter = Router();
 
 playRouter.get('/song/:songName', async (req, res) => {
 	const { songName } = req.params;
-	const deviceID = await playSong(songName);
-	res.send(deviceID);
+	await playSong(songName);
+	res.send('Now playing: ' + songName);
 });
 
+playRouter.get('/album/:albumName', async (req, res) => {
+	const { albumName } = req.params;
+	const album = await playAlbum(albumName);
+	res.status(200).json(album);
+});
+
+playRouter.get('/artist/:artistName', async (req, res) => {
+	const { artistName } = req.params;
+	const artist = await playArtist(artistName);
+	res.status(200).json(artist);
+});
 export default playRouter;
--- backend/src/services/spotifyService.ts
+++ backend/src/services/spotifyService.ts
@@ -44,6 +44,12 @@ const spotifyApi = new SpotifyWebApi();
 export const searchSong = async (songName: string) => {
 	return await spotifyApi.searchTracks(songName);
 };
+export const searchAlbum = async (albumName: string) => {
+	return await spotifyApi.searchAlbums(albumName);
+};
+export const searchArtist = async (artistName: string) => {
+	return await spotifyApi.searchArtists(artistName);
+};
 
 export const setAccessToken = (accessToken: string) => {
 	spotifyApi.setAccessToken(accessToken);
@@ -111,4 +117,23 @@ export const playSong = async (songName: string) => {
 	const songURI = results.body.tracks.items[0].uri;
 	await spotifyApi.play({ uris: [songURI] });
 	await setDevice();
+};
+
+export const playAlbum = async (albumName: string) => {
+	const results = await searchAlbum(albumName);
+	if (results === undefined) return;
+	const albumURI = results.body.albums.items[0].uri;
+	await spotifyApi.play({ context_uri: albumURI });
+	await setDevice();
+	return results;
+};
+
+export const playArtist = async (albumName: string) => {
+	const results = await searchArtist(albumName);
+	if (results === undefined) return;
+	const albumURI = results.body.artists.items[0].uri;
+	await spotifyApi.setShuffle(true);
+	await spotifyApi.play({ context_uri: albumURI });
+	//await setDevice();
+	return results;
 };
