6fdea286bc345030f97898352548b841955e2fb8
Oscar Palm <palmoscar4@gmail.com>
2023-02-13 17:19:08 +0100

    Added play controls

--- frontend/components/PlayController/PlayController.tsx
+++ frontend/components/PlayController/PlayController.tsx
@@ -1,43 +1,52 @@
-import { pauseMusic, playMusic } from '@/lib/MusicController'
-import FastForwardIcon from '@mui/icons-material/FastForward'
-import FastRewindIcon from '@mui/icons-material/FastRewind'
-import PauseIcon from '@mui/icons-material/Pause'
-import PlayArrowIcon from '@mui/icons-material/PlayArrow'
-import { useState } from 'react'
-import styles from './PlayController.module.css'
+import {
+  nextSong,
+  pauseMusic,
+  playMusic,
+  previousSong,
+} from "@/lib/MusicController";
+import FastForwardIcon from "@mui/icons-material/FastForward";
+import FastRewindIcon from "@mui/icons-material/FastRewind";
+import PauseIcon from "@mui/icons-material/Pause";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import { useState } from "react";
+import styles from "./PlayController.module.css";
 
 const PlayController = () => {
-	const [isPlaying, setIsPlaying] = useState(false)
-	return (
-		<div className={styles.playControllerBox}>
-			<FastRewindIcon
-				className='icon'
-				onClick={() => setIsPlaying(!isPlaying)}
-			/>
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div className={styles.playControllerBox}>
+      <FastRewindIcon
+        className="icon"
+        onClick={() => {
+          previousSong();
+        }}
+      />
 
-			{isPlaying ? (
-				<PauseIcon
-					className='icon'
-					onClick={() => {
-						setIsPlaying(false)
-						pauseMusic()
-					}}
-				/>
-			) : (
-				<PlayArrowIcon
-					className='icon'
-					onClick={() => {
-						setIsPlaying(true)
-						playMusic()
-					}}
-				/>
-			)}
+      {isPlaying ? (
+        <PauseIcon
+          className="icon"
+          onClick={() => {
+            setIsPlaying(false);
+            pauseMusic();
+          }}
+        />
+      ) : (
+        <PlayArrowIcon
+          className="icon"
+          onClick={() => {
+            setIsPlaying(true);
+            playMusic();
+          }}
+        />
+      )}
 
-			<FastForwardIcon
-				className='icon'
-				onClick={() => setIsPlaying(!isPlaying)}
-			/>
-		</div>
-	)
-}
-export default PlayController
+      <FastForwardIcon
+        className="icon"
+        onClick={() => {
+          nextSong();
+        }}
+      />
+    </div>
+  );
+};
+export default PlayController;
--- frontend/components/SongResult/SongResult.tsx
+++ frontend/components/SongResult/SongResult.tsx
@@ -1,6 +1,7 @@
 import styles from './SongResult.module.css'
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import QueueIcon from "@mui/icons-material/Queue";
+import { startSong } from "@/lib/MusicController";
 export interface SongContainerInterface {
   name: string;
   album: string;
@@ -8,26 +9,6 @@ export interface SongContainerInterface {
   length: string;
   songUri: string;
 }
-
-const playSong = async (song: string) => {
-  try {
-    const response = await fetch(`http://localhost:8080/api/play/song`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ songURI: song }),
-    });
-    const json = await response.json();
-    if (json.status === 200) {
-      console.log("Song played");
-    } else {
-      throw Error("Song could not be played");
-    }
-  } catch (error: any) {
-    console.log(error);
-  }
-};
 
 const addQueue = async (song: string) => {
   try {
@@ -68,7 +49,7 @@ const SongContainer = ({
         <PlayArrowIcon
           className={`icon ${styles.resultIcon}`}
           onClick={() => {
-            playSong(songUri);
+            startSong(songUri);
           }}
         />
         <QueueIcon
