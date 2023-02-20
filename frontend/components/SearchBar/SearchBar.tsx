34265aa542b7fa3094122896b213c28a6b9b2836
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-02-20 10:56:52 +0100

    Search now only active when focused or has content

--- frontend/components/SearchBar/SearchBar.tsx
+++ frontend/components/SearchBar/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import styles from './SearchBar.module.css'
 export interface SearchBarInterface {
 	setSearch: (search: boolean) => void
@@ -10,16 +11,31 @@ const SearchBar = ({
 	searchText,
 	setSearchText
 }: SearchBarInterface) => {
+	const searchRef = useRef<HTMLInputElement>(null)
+
+	const updateSearch = () => {
+		if (searchText !== '' || document.activeElement === searchRef.current)
+			setSearch(true)
+		else setSearch(false)
+	}
+
 	return (
 		<input
 			className={styles.searchBar}
 			type='text'
 			placeholder='Search...'
+			ref={searchRef}
 			onFocus={() => {
-				setSearch(true)
+				updateSearch()
+			}}
+			onBlur={() => {
+				updateSearch()
 			}}
 			value={searchText}
-			onChange={(e) => setSearchText(e.target.value)}
+			onChange={(e) => {
+				setSearchText(e.target.value)
+				updateSearch()
+			}}
 		/>
 	)
 }
