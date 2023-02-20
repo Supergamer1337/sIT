75649490d17b51c9f49a76b7caae686b6195a247
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-02-20 12:03:38 +0100

    Now transitioning search bar

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -1,19 +1,46 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Search from '../Search/Search'
+import styles from './Header.module.css'
+
+const useHeightAnimation = () => {
+	const titleRef = useRef<HTMLDivElement>(null)
+	const [initialisedHeight, setInitialisedHeight] = useState(false)
+
+	useEffect(() => {
+		if (!titleRef.current) return
+		titleRef.current.style.setProperty(
+			'--maxHeight',
+			titleRef.current.getBoundingClientRect().height + 'px'
+		)
+		setInitialisedHeight(true)
+	}, [])
+
+	return { titleRef, initialisedHeight }
+}
+
 const Header = () => {
 	const [search, setSearch] = useState(false)
+	const { titleRef, initialisedHeight } = useHeightAnimation()
+
 	return (
 		<header>
-			{search ? null : (
-				<>
-					<h1>
-						sp
-						<Image src='/Logo.png' alt='spIT!' width={64} height={64} />!
-					</h1>
-					<br />
-				</>
-			)}
+			<div
+				ref={titleRef}
+				className={
+					// I don't like this ternary, but it works. FIX: Refactor
+					initialisedHeight
+						? search
+							? styles.titleHidden
+							: styles.titleShown
+						: ''
+				}>
+				<h1>
+					sp
+					<Image src='/Logo.png' alt='spIT!' width={64} height={64} />!
+				</h1>
+				<br />
+			</div>
 			<Search setSearch={setSearch} />
 		</header>
 	)
