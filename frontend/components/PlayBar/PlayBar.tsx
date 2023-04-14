c372519ec7955d3088e22b31ccac5f2a7b242375
Oscar Palm <palmoscar4@gmail.com>
2023-04-14 16:43:14 +0200

    it doesn't look worse

--- frontend/components/PlayBar/PlayBar.tsx
+++ frontend/components/PlayBar/PlayBar.tsx
@@ -4,21 +4,34 @@ import PlayController from '../PlayController/PlayController'
 import styles from './PlayBar.module.css'
 
 const PlayBar = () => {
-	const [nowPlaying, setNowPlaying] = useState<string>('')
+	const [nowPlaying, setNowPlaying] = useState({
+    name: "",
+    artists: [""],
+    album: "",
+  });
 
-	useWebsocketListenAndEmit(
-		'update-playback-state',
-		(data) => {
-			setNowPlaying(data.name)
-		},
-		'get-playback-state',
-		[]
-	)
+  useWebsocketListenAndEmit(
+    "update-playback-state",
+    (data) => {
+      setNowPlaying({
+        name: data.name,
+        artists: data.artist,
+        album: data.album,
+      });
+    },
+    "get-playback-state",
+    []
+  );
 
-	return (
-		<div className={styles.playBar}>
-			<PlayController /> Now playing: {nowPlaying}
-		</div>
-	)
+  return (
+    <div className={styles.playBar}>
+      <PlayController />{" "}
+      <>
+        Now playing: {nowPlaying.name}
+        <br />
+        by: {nowPlaying.artists.join(", ")} ({nowPlaying.album})
+      </>
+    </div>
+  );
 }
 export default PlayBar
