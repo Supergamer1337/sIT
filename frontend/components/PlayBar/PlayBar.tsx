851ba80f132d9ee49cdb447c61d185d489ed6ec8
Oscar Palm <palmoscar4@gmail.com>
2023-04-14 17:06:50 +0200

    fix for fix of fix

--- frontend/components/PlayBar/PlayBar.tsx
+++ frontend/components/PlayBar/PlayBar.tsx
@@ -6,30 +6,42 @@ import styles from './PlayBar.module.css'
 const PlayBar = () => {
 	const [nowPlaying, setNowPlaying] = useState({
     name: "",
+    artists: [""],
+    album: "",
+  });
+  const [playbackState, setPlaybackState] = useState({
+    name: "",
     artists: "",
     album: "",
   });
-
   useWebsocketListenAndEmit(
     "update-playback-state",
     (data) => {
       setNowPlaying({
         name: data.name,
-        artists: data.artist.join(", "),
+        artists: data.artist,
         album: data.album,
       });
     },
     "get-playback-state",
     []
   );
-
+  setInterval(() => {
+    if (nowPlaying.artists) {
+      setPlaybackState({
+        name: nowPlaying.name,
+        artists: nowPlaying.artists.join(", "),
+        album: nowPlaying.album,
+      });
+    }
+  }, 100);
   return (
     <div className={styles.playBar}>
       <PlayController />{" "}
       <>
-        Now playing: {nowPlaying.name}
+        Now playing: {playbackState.name}
         <br />
-        by: {nowPlaying.artists} ({nowPlaying.album})
+        by: {playbackState.artists} ({playbackState.album})
       </>
     </div>
   );
