8156d87c8ad0ae8eff27bc0f7896d066fe4726c7
Oscar Palm <55193662+x183@users.noreply.github.com>
2023-07-25 07:45:25 +0200

    Commented search.tsx

--- frontend/components/Search/Search.tsx
+++ frontend/components/Search/Search.tsx
@@ -28,7 +28,7 @@ const searchHook = (searchValue: string) => {
           },
           body: JSON.stringify({
             name: searchValue,
-            amount: 20 /* document.body.clientHeight / 50 */,
+            amount: 20 /* document.body.clientHeight / 50 */, // The formula works, kind of, leave it commented out until further improved
           }),
         });
         const json = await response.json();
